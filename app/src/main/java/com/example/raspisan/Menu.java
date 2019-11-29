23ecfb995980bd4715db4981229753b312738b77
NovichkovaMaria <0512masha@gmail.com>
2019-11-30 00:31:24 +0400

    added receiver added gui

--- app/src/main/java/com/example/raspisan/Menu.java
+++ app/src/main/java/com/example/raspisan/Menu.java
@@ -1,17 +1,13 @@
 package com.example.raspisan;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
-    /*@Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }*/
-    @Override
+public class Menu extends AppCompatActivity {
+
+   /* @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
@@ -37,9 +33,5 @@ public class MainActivity extends AppCompatActivity {
             default:
                 break;
         }
-    }
-
-    public void onClick_Set(View view) {
-
-    }
+    }*/
 }
