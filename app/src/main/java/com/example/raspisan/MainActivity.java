b3c99f09045578e47224fa75a245a70ad0d13154
NovichkovaMaria <0512masha@gmail.com>
2019-11-29 14:32:47 +0400

    Initial commit

---
+++ app/src/main/java/com/example/raspisan/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.raspisan;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+}
