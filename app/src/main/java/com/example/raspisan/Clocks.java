8dc41496c6a2cda2e3193f9286348933e6347339
NovichkovaMaria <0512masha@gmail.com>
2019-11-30 01:04:24 +0400

    Add files

---
+++ app/src/main/java/com/example/raspisan/Clocks.java
@@ -0,0 +1,13 @@
+package com.example.raspisan;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class Clocks extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/main/java/com/example/raspisan/Raspisan.java
@@ -0,0 +1,13 @@
+package com.example.raspisan;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class Raspisan extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_raspisan);
+    }
+}
