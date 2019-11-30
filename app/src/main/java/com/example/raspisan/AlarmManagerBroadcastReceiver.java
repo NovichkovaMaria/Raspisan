116ef89e8c95c05b945a3b80cd2a4905d038aadc
SpokyOky <a4ottruba@gmail.com>
2019-11-30 15:26:55 +0400

    add alarm repair fix

--- app/src/main/java/com/example/raspisan/AlarmManagerBroadcastReceiver.java
+++ app/src/main/java/com/example/raspisan/AlarmManagerBroadcastReceiver.java
@@ -6,22 +6,57 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.os.PowerManager;
+import android.util.Log;
+import android.widget.Toast;
+
+import java.util.Calendar;
+import java.util.Date;
 
 public class AlarmManagerBroadcastReceiver extends BroadcastReceiver {
+
+    private String tag = "AlarmManager";
+
+    public AlarmManagerBroadcastReceiver(){
+    }
+
     @Override
     public void onReceive(Context context, Intent intent) {
         PowerManager powerManager = (PowerManager)context.getSystemService(Context.POWER_SERVICE);
         PowerManager.WakeLock wakeLock = powerManager.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK,
                 "raspisan:wakelock");
         wakeLock.acquire();
-        //
+        Log.d(tag, "onReceive WakeLock");
+
         wakeLock.release();
     }
 
-    public void SetAlarm(Context context){
+    public void SetAlarm(Context context, Calendar begin){
+        Log.d(tag, "SetAlarm");
         AlarmManager alarmManager = (AlarmManager)context.getSystemService(Context.ALARM_SERVICE);
         Intent intent = new Intent(context, AlarmManagerBroadcastReceiver.class);
-        PendingIntent pendingIntent = PendingIntent.getBroadcast(context, 0, intent, 0);
-        alarmManager.setRepeating(AlarmManager.RTC_WAKEUP, System.currentTimeMillis(), 1000 * 5, pendingIntent);
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(context, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+        alarmManager.set(AlarmManager.RTC_WAKEUP, begin.getTimeInMillis(), pendingIntent);
+
+        Log.d(tag, "Month = " + Integer.toString(begin.get(Calendar.MONTH) + 1));
+        Log.d(tag, "Day = " + Integer.toString(begin.get(Calendar.DAY_OF_MONTH)));
+        Log.d(tag, "Hour = " + Integer.toString(begin.get(Calendar.HOUR_OF_DAY)));
+        Log.d(tag, "Minute = " + Integer.toString(begin.get(Calendar.MINUTE)));
+
+        String output = Integer.toString(begin.get(Calendar.DAY_OF_MONTH)) + "." + Integer.toString(begin.get(Calendar.MONTH)) +
+                "\n" + Integer.toString(begin.get(Calendar.HOUR_OF_DAY)) + ":" + Integer.toString(begin.get(Calendar.MINUTE));
+        Toast toast = Toast.makeText(context, output, Toast.LENGTH_LONG);
+        toast.show();
+    }
+
+    public void CancelAlarm(Context context){
+        Log.d(tag, "CancelAlarm");
+        Intent intent = new Intent(context, AlarmManagerBroadcastReceiver.class);
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(context, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+        AlarmManager alarmManager = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
+        alarmManager.cancel(pendingIntent);
+        Toast toast = Toast.makeText(context, "Будильник выключен", Toast.LENGTH_SHORT);
+        toast.show();
+
     }
 }
--- app/src/main/java/com/example/raspisan/ClocksActivity.java
+++ app/src/main/java/com/example/raspisan/ClocksActivity.java
@@ -1,22 +1,94 @@
 package com.example.raspisan;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
 
 public class ClocksActivity extends AppCompatActivity {
+
+    private String tag = "ClockActivity";
+    private AlarmManagerBroadcastReceiver alarmReceiver;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_clocks);
+        alarmReceiver = new AlarmManagerBroadcastReceiver();
     }
 
     public void onClick_SetAlarm(View view){
+        Log.d(tag, "onClick_Set");
+        Context context = this.getApplicationContext();
+        if (alarmReceiver != null){
+            Calendar now = Calendar.getInstance();
+            Calendar begin = Calendar.getInstance();
+            RadioGroup radioGroup = (RadioGroup)findViewById(R.id.radioGroup);
+            int i = radioGroup.getCheckedRadioButtonId();
+            RadioButton rb = (RadioButton)findViewById(i);
+            Log.d(tag, rb.toString());
+            switch(rb.getId()){
+                case R.id.radioButton_1:
+                    begin.set(now.get(Calendar.YEAR), now.get(Calendar.MONTH),
+                            now.get(Calendar.DAY_OF_MONTH), 8, 0, 0);
+                    break;
+                case R.id.radioButton_2:
+                    begin.set(now.get(Calendar.YEAR), now.get(Calendar.MONTH),
+                            now.get(Calendar.DAY_OF_MONTH), 9, 40, 0);
+                    break;
+                case R.id.radioButton_3:
+                    begin.set(now.get(Calendar.YEAR), now.get(Calendar.MONTH),
+                            now.get(Calendar.DAY_OF_MONTH), 11, 30, 0);
+                    break;
+                case R.id.radioButton_4:
+                    begin.set(now.get(Calendar.YEAR), now.get(Calendar.MONTH),
+                            now.get(Calendar.DAY_OF_MONTH), 13, 10, 0);
+                    break;
+                case R.id.radioButton_5:
+                    begin.set(now.get(Calendar.YEAR), now.get(Calendar.MONTH),
+                            now.get(Calendar.DAY_OF_MONTH), 14,50, 0);
+                    break;
+                case R.id.radioButton_6:
+                    begin.set(now.get(Calendar.YEAR), now.get(Calendar.MONTH),
+                            now.get(Calendar.DAY_OF_MONTH), 16, 30, 0);
+                    break;
+                case R.id.radioButton_7:
+                    begin.set(now.get(Calendar.YEAR), now.get(Calendar.MONTH),
+                            now.get(Calendar.DAY_OF_MONTH), 18, 10, 0);
+                    break;
+                case R.id.radioButton_8:
+                    begin.set(now.get(Calendar.YEAR), now.get(Calendar.MONTH),
+                            now.get(Calendar.DAY_OF_MONTH), 19, 50, 0);
+                    break;
+                default:
+                    Log.d(tag, "default");
+                    break;
+            }
+            if (begin.before(now)){
+                begin.add(Calendar.DAY_OF_MONTH, 1);
+                Log.d(tag, "begin before now");
+            }
 
+            alarmReceiver.SetAlarm(context, begin);
+        } else{
+            Log.d(tag + " set", "Alarm is null");
+        }
     }
 
     public void onClick_DeleteAlarm(View view){
-
+        Log.d(tag, "onClick_Delete");
+        Context context = this.getApplicationContext();
+        if (alarmReceiver != null){
+            alarmReceiver.CancelAlarm(context);
+        } else {
+            Log.d(tag + " cancel", "Alarm is null");
+        }
     }
 }
