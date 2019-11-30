add65aa0e3d64dbfb139ba0b271ab770dc86ea36
SpokyOky <a4ottruba@gmail.com>
2019-11-30 09:49:16 +0400

    reworked gui

--- app/src/main/java/com/example/raspisan/ClocksActivity.java
+++ app/src/main/java/com/example/raspisan/ClocksActivity.java
@@ -2,12 +2,21 @@ package com.example.raspisan;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class ClocksActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_clocks);
+    }
+
+    public void onClick_SetAlarm(View view){
+
+    }
+
+    public void onClick_DeleteAlarm(View view){
+
     }
 }
